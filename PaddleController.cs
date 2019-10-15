bbcf7927ed3c84968e8e3a2a03f796d73f03d6ec
Pyromance14 <45087708+Pyromance14@users.noreply.github.com>
2019-10-15 09:26:59 -0400

    Add files via upload

---
+++ PaddleController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+
+    public float speed = 10.0f;
+    public float boundY = 2.25f;
+
+    private Rigidbody2D rg;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rg = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vel = rg.velocity;
+        if (Input.GetKey(moveUp))
+        {
+            vel.y = speed;
+        }
+        else if (Input.GetKey(moveDown))
+        {
+            vel.y = -speed;
+        }
+        else
+        {
+            vel.y = 0;
+        }
+        rg.velocity = vel;
+
+        var pos = transform.position;
+        if (pos.y > boundY)
+        {
+            pos.y = boundY;
+        }
+        else if (pos.y < -boundY)
+        {
+            pos.y = -boundY;
+        }
+        transform.position = pos;
+    }
+}
